493535e87d7109584c2d297c89bf89cd3ea694a5
Peder Lillelien <peder.lillelien@gmail.com>
2025-04-20 14:24:10 +0200

    feat: :sparkles: started work on hangman game
    started work on html drawing of hangman

---
+++ src/app/games/hangman/hangmanDrawing.tsx
@@ -0,0 +1,126 @@
+const head = (
+  <div
+    style={{
+      width: "50px",
+      height: "50px",
+      borderRadius: "100%",
+      border: "10px solid black",
+      position: "absolute",
+      top: "49px",
+      right: "-20px",
+    }}
+  />
+);
+
+const body = (
+  <div
+    style={{
+      width: "10px",
+      height: "100px",
+      background: "black",
+      position: "absolute",
+      top: "98px",
+      right: 0,
+    }}
+  />
+);
+
+const right_arm = (
+  <div
+    style={{
+      width: "100px",
+      height: "10px",
+      background: "black",
+      position: "absolute",
+      top: "100px",
+      right: "-90px",
+      rotate: "-30deg",
+      transformOrigin: "leftBottom",
+    }}
+  />
+);
+
+const left_arm = (
+  <div
+    style={{
+      width: "100px",
+      height: "10px",
+      background: "black",
+      position: "absolute",
+      top: "100px",
+      right: "0px",
+      rotate: "30deg",
+      transformOrigin: "rightBottom",
+    }}
+  />
+);
+
+const left_leg = (
+  <div
+    style={{
+      width: "100px",
+      height: "10px",
+      background: "black",
+      position: "absolute",
+      top: "212px",
+      right: "0px",
+      rotate: "-30deg",
+      transformOrigin: "rightBottom",
+    }}
+  />
+);
+
+const right_leg = (
+  <div
+    style={{
+      width: "100px",
+      height: "10px",
+      background: "black",
+      position: "absolute",
+      top: "212px",
+      right: "-90px",
+      rotate: "-150deg",
+      transformOrigin: "leftBottom",
+    }}
+  />
+);
+
+export function HangmanDrawing() {
+  return (
+    <div style={{ position: "relative" }}>
+      {head}
+      {body}
+      {right_arm}
+      {left_arm}
+      {left_leg}
+      {right_leg}
+      <div
+        style={{
+          position: "absolute",
+          height: "50px",
+          width: "10px",
+          background: "black",
+          top: 0,
+          right: 0,
+        }}
+      />
+      <div
+        style={{
+          height: "10px",
+          width: "200px",
+          background: "black",
+          marginLeft: "120px",
+        }}
+      />
+      <div
+        style={{
+          height: "400px",
+          width: "10px",
+          background: "black",
+          marginLeft: "120px",
+        }}
+      />
+      <div style={{ height: "10px", width: "250px", background: "black" }} />
+    </div>
+  );
+}
--- src/app/games/hangman/page.tsx
+++ src/app/games/hangman/page.tsx
@@ -1,12 +1,24 @@
+"use client";
+import { useState } from "react";
+import wordList from "./wordList.json";
+import "./hangman.css";
+import { HangmanDrawing } from "./hangmanDrawing";
+import { HangmanWord } from "./hangmanWord";
+import { Keyboard } from "./keyboard";
+
 export default function hangman() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return wordList[Math.floor(Math.random() * wordList.length)];
+  });
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
   return (
     <>
-      <div className="">
-        <img
-          className="w-full"
-          src="/images/UnderConstruction.png"
-          alt="Sunset in the mountains"
-        />
+      <div className="hangman">
+        <div className="win-loose">loose win</div>
+        <h1>{wordToGuess}</h1>
+        <HangmanDrawing></HangmanDrawing>
+        <HangmanWord></HangmanWord>
+        <Keyboard></Keyboard>
       </div>
     </>
   );
